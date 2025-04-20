d906f4159d743f47a42d4e67461e98c7182c3db2
Vidal777rj <michelvidal65@outlook.com>
2025-04-19 22:11:26 -0300

    iniciando Projeto

---
+++ 1_controle-financeiro/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace _1_controle_financeiro.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
