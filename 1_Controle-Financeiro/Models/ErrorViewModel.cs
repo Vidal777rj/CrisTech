815cc68104b29d1c81cb83bfb8a2aec9f902153e
Vidal777rj <michelvidal65@outlook.com>
2025-04-19 22:21:20 -0300

    Iniciando

---
+++ 1_Controle-Financeiro/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace _1_Controle_Financeiro.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
